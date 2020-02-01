679963aa6f45f9f5e1ab63aedb713b4c97ca5a5f
Héricles Emanuel <hericles.me@gmail.com>
2020-02-01 11:22:20 -0300

    add toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatMenuModule} from '@angular/material/menu';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import {MatMenuModule} from '@angular/material/menu';
     MatMenuModule,
     BrowserModule,
     AppRoutingModule,
+    MatToolbarModule,
     BrowserAnimationsModule
   ],
   providers: [],
